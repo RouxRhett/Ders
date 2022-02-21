1dc19c0ef074ec84b96ae91366c22a283e379ae2
RouxRhett <nsch.4871@gmail.com>
2022-02-21 14:04:02 +0900

    [change]Twitterに変更

--- app/controllers/public/users/omniauth_callbacks_controller.rb
+++ app/controllers/public/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,13 @@
 class Public::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def google_oauth2
-    callback_for(:google)
+  def twitter
+    callback_for(:twitter)
   end
 
   # common callback method
   def callback_for(provider)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
