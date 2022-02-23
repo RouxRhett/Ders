e755f79feaa6d90e75c7536f6d7ad9946428821b
RouxRhett <nsch.4871@gmail.com>
2022-02-24 07:12:14 +0900

    [fix]sns認証後のリダイレクトページを変更

--- app/controllers/public/users/omniauth_callbacks_controller.rb
+++ app/controllers/public/users/omniauth_callbacks_controller.rb
@@ -7,8 +7,9 @@ class Public::Users::OmniauthCallbacksController < Devise::OmniauthCallbacksCont
   def callback_for(service)
     @user = User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication # this will throw if @user is not activated
+      sign_in @user, event: :authentication # this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "#{service}".capitalize) if is_navigational_format?
+      redirect_to mypage_path
     else
       session["devise.#{service}_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_url
