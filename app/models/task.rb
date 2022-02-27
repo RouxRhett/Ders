1ee3dcdf1636d061cdbd4fdd7fb58497131c8c7b
RouxRhett <nsch.4871@gmail.com>
2022-02-27 13:07:02 +0900

    [fix]Taskモデルのバリデーション追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,4 +3,7 @@ class Task < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
+  validates :time, presence: true, numericality: {
+    only_integer: true, greater_than_or_equal_to: 1,
+  }
 end
