86ddbb428b408f68e78098c3e1ce6260c2de1d6b
RouxRhett <nsch.4871@gmail.com>
2022-03-21 20:10:42 +0900

    [fix][change]実績解除の一覧取得のメソッドを一部変更、ヘッダーに実績の項目を追加

--- app/controllers/public/unlock_lists_controller.rb
+++ app/controllers/public/unlock_lists_controller.rb
@@ -2,6 +2,6 @@ class Public::UnlockListsController < ApplicationController
   before_action :user_login_check
 
   def index
-    @unlock_lists = current_user.unlock_lists
+    @unlock_lists = current_user.unlock_lists.includes([:achievement])
   end
 end
